d7b44096fd4bc8e13b60e5b355e4047c7472d231
AJ <aj@avironsoftware.com>
2022-05-03 22:31:54 -0500

    Series/Event creation

--- Pages/Series/Create.cshtml.cs
+++ Pages/Series/Create.cshtml.cs
@@ -1,12 +1,54 @@
+using System;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OpenDataShare;
 
 namespace LEPS.Pages.Series
 {
     public class Create : PageModel
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Create(
+            ApplicationDbContext dbContext
+        )
+        {
+            _dbContext = dbContext;
+        }
         public void OnGet()
         {
             
+        }
+
+        public void OnPostCreateSeries()
+        {
+            var inputCount = Request.Form.Keys.Count;
+            var inputKeys = Request.Form.Keys;
+            var seriesName = Request.Form["seriesName"];
+            var seriesDesc = Request.Form["seriesDesc"];
+
+            var newSeries = new Entities.Series
+            {
+                Name = seriesName,
+                Desc = seriesDesc
+            };
+            _dbContext.Add(newSeries);
+            _dbContext.SaveChanges();
+
+            for (var i = 0; i < (inputCount - 3)/3; i++)
+            {
+                var seriesEvent = new Entities.Event
+                {
+                    Name = Request.Form[$"eventName{i}"],
+                    Cost = Convert.ToDecimal(Request.Form[$"eventCost{i}"]),
+                    DateTime = Convert.ToDateTime(Request.Form[$"eventDate{i}"]),
+                    Series = newSeries
+                    
+                };
+                _dbContext.Add(seriesEvent);
+                
+            }
+            _dbContext.SaveChanges();
         }
     }
 }
