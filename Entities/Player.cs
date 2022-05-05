dd5932a306fcc1bae21d44a9ed0bac1362a2e2b7
AJ <aj@avironsoftware.com>
2022-05-04 21:12:15 -0500

    talent-create

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -1,9 +1,14 @@
+using System;
+
 namespace LEPS.Entities
 {
     public class Player
     {
         public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public decimal Bankroll { get; set; }
+        public DateTime CreateDate { get; set; }
     }
 }
