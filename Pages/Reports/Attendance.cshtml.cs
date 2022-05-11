9fb055105c4f1884645d4a10f3ba8ed61f0586a1
AJ <aj@avironsoftware.com>
2022-05-10 21:41:37 -0500

    Attendance Report

---
+++ Pages/Reports/Attendance.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LEPS.Pages.Reports
+{
+    public class Attendance : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
